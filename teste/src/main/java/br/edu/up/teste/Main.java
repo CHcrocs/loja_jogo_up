44999534c826c36b9aed0c624093733f3fdd5bef
CHcrocs <138597970+CHcrocs@users.noreply.github.com>
2024-05-23 11:03:04 -0300

    criação classes de modelo

--- teste/src/main/java/br/edu/up/teste/Main.java
+++ teste/src/main/java/br/edu/up/teste/Main.java
@@ -1,11 +1,48 @@
 package br.edu.up.teste;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import br.edu.up.teste.utils.Util;
+import br.edu.up.teste.views.TesteView;
+
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int op;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("#############################");
+            System.out.println("#           Menu            #");
+            System.out.println("#############################");
+            System.out.println("0 - SAIR");
+            System.out.println("1 - Lista");
+
+            op = Util.lerOpcao(scanner);
+            exibirView(scanner, op);
+
+        } while (op != 0);
     }
+
+    private static void exibirView(Scanner scanner, int op) {
+        switch (op) {
+            case 0:
+                System.out.println("Até mais");
+                break;
+            case 1:
+                TesteView.iniciar(scanner);
+                break;
+            case 99:
+                System.out.println("Informe Um valor Valido");
+            break;
+            default:
+                System.out.println("Opção Invalida!");
+                break;
+        }
+    }
+    
 }
---
+++ teste/src/main/java/br/edu/up/teste/views/TesteView.java
@@ -0,0 +1,26 @@
+package br.edu.up.teste.views;
+
+import java.util.Scanner;
+
+/**
+ * TesteView
+ */
+public class TesteView {
+
+    public static void iniciar(Scanner scanner) {
+        int op;
+
+        do {
+        System.out.println("#############################");
+        System.out.println("#         TesteList         #");
+        System.out.println("#############################");
+        System.out.println("0 - SAIR");
+        System.out.println("1 - Listar");
+        System.out.println("2 - Cadastrar");
+        System.out.println("3 - Alterar");
+        System.out.println("4 - Remover");
+
+        op = scanner.nextInt();
+        }while(op != 0);
+    }
+}
