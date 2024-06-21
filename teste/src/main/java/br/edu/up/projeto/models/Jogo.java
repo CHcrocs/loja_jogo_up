59e748e6f6d2d8919de9e066c73862c07c87b15a
Crocs <138597970+CHcrocs@users.noreply.github.com>
2024-06-21 20:41:18 -0300

    .

--- teste/src/main/java/br/edu/up/projeto/models/Jogo.java
+++ teste/src/main/java/br/edu/up/projeto/models/Jogo.java
@@ -2,10 +2,10 @@ package br.edu.up.projeto.models;
 
 abstract public class Jogo {
 
-    public String nome;
-    public String genero;
-    public String classificacao;
-    public Double preco;
+    private String nome;
+    private String genero;
+    private String classificacao;
+    private Double preco;
 
     public Jogo(String nome, String genero, String classificacao, Double preco) {
         this.nome = nome;
@@ -14,4 +14,38 @@ abstract public class Jogo {
         this.preco = preco;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(String classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    
+    
 }
